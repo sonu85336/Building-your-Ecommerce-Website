e9355cd3b8f40720fb713c9986f4ff60d6fc5a20
sonu85336 <sonukushwaha098@gmail.com>
2023-01-06 13:02:36 +0530

    task6with async

--- src/AppApiFetch.js
+++ src/AppApiFetch.js
@@ -5,23 +5,20 @@ import "./App.css";
 
 function AppApiFetch() {
   const [movies, setMovies] = useState([]);
-  const ApiHandler = () => {
-    fetch("https://swapi.dev/api/films/")
-      .then((response) => {
-      return   response.json();
-      })
-      .then((data) => {
-        const transformdata = data.results.map((movieList) => {
-          return {
-            id: movieList.episode_id,
-            title: movieList.title,
-            releaseDate: movieList.release_date,
-            openingText: movieList.opening_crawl,
-          };
-        });
-        setMovies(transformdata);
-      });
-  };
+  async function ApiHandler() {
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
+
+    const transformdata = data.results.map((movieList) => {
+      return {
+        id: movieList.episode_id,
+        title: movieList.title,
+        releaseDate: movieList.release_date,
+        openingText: movieList.opening_crawl,
+      };
+    });
+    setMovies(transformdata);
+  }
 
   return (
     <React.Fragment>
