5dcd0397fea16b77bbc178187a0f15c4a854ad6f
sonu85336 <sonukushwaha098@gmail.com>
2023-01-10 18:39:19 +0530

    task16index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import {BrowserRouter}  from 'react-router-dom'
-import React from 'react';
 import ReactDOM from 'react-dom/client';
- 
+import { BrowserRouter } from 'react-router-dom';
+ import AuthContProvider from './authSotre/authcontProvider'
 import './index.css';
-import App from './App';
-// import bootstarp 
-import 'react-bootstrap/dist/react-bootstrap'
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
-
+import App from './AuthApp';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<BrowserRouter><App /></BrowserRouter>);
+  
+  <AuthContProvider>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter></AuthContProvider>
+);
