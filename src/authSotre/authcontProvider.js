60f915c7bc05a712642fc1f79994a644d2f58d1a
sonu85336 <sonukushwaha098@gmail.com>
2023-01-11 12:38:44 +0530

    task19Addressing security and logout issues

--- src/authSotre/authcontProvider.js
+++ src/authSotre/authcontProvider.js
@@ -2,24 +2,28 @@ import { useState } from "react";
 import AuthCartContext from "./authcontext";
 
 const AuthContProvider = (props) =>{
-   const [ token,setToken] = useState(null)
+    const initialToken = localStorage.getItem('token')
+   const [ token,setToken] = useState(initialToken)
    
    const userIsLoggedIn = !!token;
    
    const loginHandler =(token)=>{
     setToken(token);
+    localStorage.setItem('token',token)
    }
 const logoutHandler =()=>{
     setToken(null)
+    localStorage.removeItem('token')
 }
   
     const  contextValue = {
-        token:token,
+       TokenTaked:token,
         isLoggedIn:userIsLoggedIn,
         login:loginHandler,
         logout:logoutHandler,
           
     }
+    // console.log('fromprovider',contextValue.tokentaken )
     
 return (
     <AuthCartContext.Provider value={contextValue}>
