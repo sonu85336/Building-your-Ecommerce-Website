da5d83a76317c5cbc1e8d075e016e697f7675166
sonu85336 <111218932+sonu85336@users.noreply.github.com>
2023-01-11 11:17:28 +0530

    task17 Updating the Old Password

---
+++ AuthForm.js
@@ -0,0 +1,107 @@
+import { useState, useRef,useContext } from "react";
+import { useHistory } from "react-router-dom";
+import classes from "./AuthForm.module.css";
+import AuthCartContext from "../../authSotre/authcontext";
+const AuthForm = (props) => {
+  const history = useHistory()
+  const authCtx = useContext(AuthCartContext)
+  const emailItnputRef = useRef();
+  const passwordInputRef = useRef();
+  const [isLogin, setIsLogin] = useState(true);
+   const [isLoading,setIsLoading] = useState(false)
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+  const submitHandler = (event) => {
+    event.preventDefault();
+     
+    const enteredEmail = emailItnputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    
+
+setIsLoading(true)
+let url;
+    if (isLogin) {
+url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDohcQdgl3-uiia8iK3LiGBuiVUcCoDsdo'
+ }
+else {
+  url = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDohcQdgl3-uiia8iK3LiGBuiVUcCoDsdo"  
+}
+  fetch(
+        url,
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true,
+          }),
+          headers: {
+            'Content-Type': "application/json",
+          },
+        }
+      ).then((res) => {
+        setIsLoading(false)
+        if (res.ok) {
+       return res.json()
+        } else {
+          return res.json().then((data) => {
+           let errorMessage = 'Authentication failed!';
+           if(data&& data.error && data.error.message){
+            errorMessage = data.error.message;
+           }
+           throw new Error(errorMessage)
+           
+          });
+        }
+      })
+.then((data)=>{
+  console.log(data.idToken
+    )
+     authCtx.login(data.idToken)
+     history.replace('/')
+})
+.catch((err)=>{
+  alert(err.message);
+})
+    
+     
+  };
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="email">Your Email</label>
+          <input type="email"  id="email" required ref={emailItnputRef}/>
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+            ref={passwordInputRef}
+          />
+        </div>
+        <div className={classes.actions}>
+        {!isLoading && <button>{isLogin ? "Login" : "Create Account"}</button>} 
+        {isLoading && <p>Sending request...</p>}
+          <button
+            type="button"
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+               
+            {isLogin  ? "Create new account" : "Login with existing account"}
+           
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
