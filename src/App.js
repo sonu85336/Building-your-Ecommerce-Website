3fb25520fdbfc5a70f2855f37a34a1f8086e6b92
sonu85336 <sonukushwaha098@gmail.com>
2023-01-13 18:32:55 +0530

    task24 ecommerce final code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React, { useContext} from "react";
+import React, { useContext,Suspense} from "react";
 
 import { Route, Switch, Redirect } from "react-router-dom";
 
-import Product1 from "./Component/Body/BodyPages/Product1";
-import Product2 from "./Component/Body/BodyPages/Product2";
-import Product3 from "./Component/Body/BodyPages/Product3";
-import Product4 from "./Component/Body/BodyPages/Product4";
-import About from "./Pages/About";
-import ContactUs from "./Pages/contactus/ContactUs";
-import Home from "./Pages/Home";
-import Store from "./Store";
-import Login from "./Pages/loginpages/Login";
+// import Product1 from "./Component/Body/BodyPages/Product1";
+// import Product2 from "./Component/Body/BodyPages/Product2";
+// import Product3 from "./Component/Body/BodyPages/Product3";
+// import Product4 from "./Component/Body/BodyPages/Product4";
+// import About from "./Pages/About";
+// import ContactUs from "./Pages/contactus/ContactUs";
+// import Home from "./Pages/Home";
+// import Store from "./Store";
+// import Login from "./Pages/loginpages/Login";
 import CartContext from "./store/cart-context";
-import axios from "axios";
+
+const Home  = React.lazy(()=>import("./Pages/Home"))
+ const Store  = React.lazy(()=>import("./Store"))
+const ContactUs = React.lazy(()=>import("./Pages/contactus/ContactUs"))
+const About = React.lazy(()=>import("./Pages/About"))
+const Login = React.lazy(()=>import("./Pages/loginpages/Login"))
+const Product1 = React.lazy(()=>import("./Component/Body/BodyPages/Product1"))
+const Product2 = React.lazy(()=>import("./Component/Body/BodyPages/Product2"))
+const  Product3 = React.lazy(()=>import("./Component/Body/BodyPages/Product3"))
+const  Product4= React.lazy(()=>import("./Component/Body/BodyPages/Product4"))
 function App() {
   const authCtx = useContext(CartContext);
    
   return (
     <div>
       <main>
+        <Suspense fallback={<p>Loading...</p>}>
         <Switch>
           <Route path="/" exact>
             <Redirect to="/Home" />
@@ -66,6 +76,7 @@ function App() {
             <Redirect to="/Login" />
           </Route>
         </Switch>
+        </Suspense>
       </main>
     </div>
   );
